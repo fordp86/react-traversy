8441268e58e2a9d348bfadf466146434c42a863a
Ford Henley <fordp86@gmail.com>
2023-08-18 15:25:11 -0500

    Updated API Url
    Updated API url to live URL

--- src/App.js
+++ src/App.js
@@ -21,7 +21,9 @@ function App() {
 
   // Fetch Tasks
   const fetchTasks = async () => {
-    const res = await fetch("http://localhost:5000/tasks");
+    const res = await fetch(
+      "https://taskapp-jsonserver-f5efffa85a40.herokuapp.com/tasks/tasks"
+    );
     const data = await res.json();
 
     return data;
@@ -29,7 +31,9 @@ function App() {
 
   // Fetch Task
   const fetchTask = async (id) => {
-    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const res = await fetch(
+      `https://taskapp-jsonserver-f5efffa85a40.herokuapp.com/tasks/${id}`
+    );
     const data = await res.json();
 
     return data;
@@ -37,13 +41,16 @@ function App() {
 
   // Add Task
   const addTask = async (task) => {
-    const res = await fetch("http://localhost:5000/tasks", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(task),
-    });
+    const res = await fetch(
+      "https://taskapp-jsonserver-f5efffa85a40.herokuapp.com/tasks",
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(task),
+      }
+    );
 
     const data = await res.json();
 
@@ -56,9 +63,12 @@ function App() {
 
   // Delete Task
   const deleteTask = async (id) => {
-    await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: "DELETE",
-    });
+    await fetch(
+      `https://taskapp-jsonserver-f5efffa85a40.herokuapp.com/tasks/${id}`,
+      {
+        method: "DELETE",
+      }
+    );
 
     setTasks(tasks.filter((task) => task.id !== id));
   };
@@ -68,13 +78,16 @@ function App() {
     const taskToToggle = await fetchTask(id);
     const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
 
-    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: "PUT",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(updTask),
-    });
+    const res = await fetch(
+      `https://taskapp-jsonserver-f5efffa85a40.herokuapp.com/tasks/${id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(updTask),
+      }
+    );
 
     const data = await res.json();
 
